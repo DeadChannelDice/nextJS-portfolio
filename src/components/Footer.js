cfa983878c718176abc609e76716a8976bbeefff
DeadChannelDice <michael.charpin@gmail.com>
2023-07-26 10:43:44 -0400

    Initial Import from previous forked repo "Portfolio-Starter-Code "

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Layout from './Layout'
+import Link from 'next/link'
+
+
+const Footer = () => {
+  return (
+    <footer className='w-full border-t-2 border-solid border-darkfont
+    font-medium text-lg'>
+        <Layout className='py-8 flex items-center justify-between'>
+            <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+            <div className='flex items-center'>
+            Built with <span className='text-primary text-2xl px-1'>&#9825;</span>by&nbsp;
+            <Link href="https://github.com/DeadChannelDice" target={"_blank"} className='underline underline-offset-2'> Mike Charpin</Link>
+            </div>
+            <Link href="https://github.com/DeadChannelDice"> Say hello</Link>
+        </Layout>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Skills.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {motion} from "framer-motion"
+
+
+const Skill = ({name, x, y}) => {
+
+    return (
+        <motion.div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark absolute" 
+        whileHover={{scale:1.3}}
+        initial={{ x:0, y:0 }}
+        whileInView={{ x:x, y:y }}
+        transition={{ duration: 1.5}}
+        viewport={{once: true}}
+        >
+            {name}
+        </motion.div>
+    )
+
+}
+
+const Skills = () => {
+  return (
+    <>
+    <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
+    <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
+        <motion.div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark" whileHover={{scale:1.05}}
+        >
+            Web
+        </motion.div>
+
+        <Skill name="html" x="-20vw" y="2vw" />
+
+        <Skill name="css" x="-5vw" y="-10vw" />
+
+        <Skill name="Javascript" x="-15vw" y="-15vw" />
+
+        <Skill name="NextJS" x="-20vw" y="12vw" />
+
+        <Skill name="Web Design" x="20vw" y="12vw" />
+
+        <Skill name="Tailwind CSS" x="18vw" y="-10vw" />
+
+
+    </div>
+    </>
+  )
+}
+
+export default Skills
