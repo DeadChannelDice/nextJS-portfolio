f7fa29b686fc772a18cd5caf2961f78219262b77
DeadChannelDice <michael.charpin@gmail.com>
2023-07-29 14:07:48 -0400

    mid-process making pages responsive.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,16 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className='w-full border-t-2 border-solid border-darkfont
-    font-medium text-lg dark:text-light'>
-        <Layout className='py-8 flex items-center justify-between'>
+    font-medium text-lg dark:text-light sm:test-base'>
+        <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
             <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-            <div className='flex items-center'>
-            Built with <span className='text-primary text-2xl px-1'>&#9825;</span>by&nbsp;
-            <Link href="https://github.com/DeadChannelDice" target={"_blank"} className='underline underline-offset-2'> Mike Charpin</Link>
+            <div className='flex items-center lg:py-2'>
+            Built with <span className='text-primary dark:text-primaryDark text-2xl px-1'>&#9825;</span>by&nbsp;
+            <Link 
+              href="https://github.com/DeadChannelDice" 
+              target={"_blank"} 
+              className='underline underline-offset-2
+            '> Mike Charpin</Link>
             </div>
             <Link href="https://github.com/DeadChannelDice"> Say hello</Link>
         </Layout>
--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -7,8 +7,9 @@ const HireMe = () => {
   return (
     <div className='fixed left-4 bottom-4
     flex items-center justify-center overflow-hidden
+    md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
     '>
-        <div className="w-48 h-auto flex items-center justify-center relative">
+        <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
             <CircularText className={"fill-dark animate-reverse-spin dark:fill-light"} />
 
             <Link href="mailto:Michael.Charpin@gmail.com" 
@@ -18,6 +19,7 @@ const HireMe = () => {
                 bg-dark text-light shadow-md border-2 border-solid border-dark w-20 h-20 rounded-full font-semibold
                 hover:bg-light hover:text-dark
                 dark:border-light
+                md:w-12 md:h-12 md:text-[10px]
               '>
                 Hire Me
             </Link>
