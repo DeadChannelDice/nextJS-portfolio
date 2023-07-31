547c8b1dc2f3c9247e72280e41b55b2af3ae5ac2
DeadChannelDice <michael.charpin@gmail.com>
2023-07-31 19:26:23 -0400

    Refining design. Making responsive.

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,23 +6,25 @@ import LiIcon from './LiIcon'
 const Details = ({type, time, place, info}) => {
     const ref = useRef(null)
     return (
-        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between'>
-
+        
+        <li ref={ref}
+            className='
+            my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between
+            border-4 border-transparent py-10 px-16 rounded-xl
+            dark:border-4 darK:border-solid dark:border-primaryDark/75 xs:w-full sm:w-full'>
             {/* <LiIcon reference={ref}/> */}
-            <motion.div 
+            <motion.div
             initial={{y:50}}
             whileInView={{y:0}}
             transition={{duration: 1, type:"spring"}}
             >
-                <h3 className='capitalize font-bold font-2xl' >{type}</h3>
-                <span className='capitaize font-medium text-dark/75 dark:text-light/50'>
+                <h3 className='font-bold font-2xl py-2' >{type}</h3>
+                <span className='capitaize font-medium text-dark/75 dark:text-light/50 py-2'>
                     {time} | {place}
                 </span>
-
-                <p className='font-medium w-full '>
+                <p className='font-medium w-full py-2 '>
                     {info}
                 </p>
-
             </motion.div>
         </li>
     )
@@ -39,11 +41,11 @@ const Education = () => {
 
   return (
     <div className='my-32'>
-        <h2 className="font-bold text-8xl mb-32 w-full text-center">
+        <h2 className="font-bold text-8xl mb-32 w-full text-center  sm:text-6xl xs:text-4xl ">
             Education
         </h2>
 
-        <div ref={ref} className='w-[75%] mx-auto relative'>
+        <div ref={ref} className='w-[75%] mx-auto relative xs:w-full sm:w-full md:w-full'>
 
             {/* <motion.div 
                 style={{scaleY: scrollYProgress}}
