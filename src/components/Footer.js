969fa64a4d9ec091f9d1afe0f4d844786603c890
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2023-09-08 17:14:39 -0400

    Changes heart emoji to wizard.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,13 +10,15 @@ const Footer = () => {
         <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
             <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
             <div className='flex items-center lg:py-2'>
-            Built with <span className='text-primary dark:text-primaryDark text-2xl px-1'>&#9825;</span>by&nbsp;
+            Built by&nbsp;
             <Link 
               href="https://github.com/DeadChannelDice" 
               target={"_blank"} 
               className='underline underline-offset-2
             '> Mike Charpin</Link>
+            <span className='text-primary dark:text-primaryDark text-2xl px-1'>&#129497;</span>
             </div>
+            
             <Link href="https://github.com/DeadChannelDice"> Say hello</Link>
         </Layout>
     </footer>
