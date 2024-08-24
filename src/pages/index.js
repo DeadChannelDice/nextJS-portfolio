94679bb0d6c29e7b261b94eaba2ee3bdd50e8365
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-08-24 12:25:27 -0400

    Changing out the main profile image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Layout from '@/components/Layout'
 import Image from 'next/image'
-import profilePic from "../../public/images/profile/charpin-profile-2023-2.jpg"
+import profilePic from "../../public/images/profile/charpin-profile-2024-01.webp"
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/components/Icons'
