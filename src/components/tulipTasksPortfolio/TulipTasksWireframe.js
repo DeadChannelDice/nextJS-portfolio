6dca313df59a277505b3ed8ebad4e725c490ffc3
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-08-29 14:49:23 -0400

    Formatting and adding animations.

--- src/components/tulipTasksPortfolio/TulipTasksWireframe.js
+++ src/components/tulipTasksPortfolio/TulipTasksWireframe.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import DesignProcess from '../ui/DesignProcess'
-import TulipTasksHeroImage from '../../../public/images/tulipTasks/tulipTasks-hero-image-01.webp'
+import { motion } from 'framer-motion'
+import AnimatedText from '@/components/AnimatedText'
+
+import Image from 'next/image'
+const FramerImage = motion(Image)
+import TulipTasksInterview from "../../../public/images/tulipTasks/User-Interview-01.png"
+import TulipTasksWireFrame from "../../../public/images/tulipTasks/TulipTasks-WireFrame.png"
 
 
 
@@ -8,26 +13,29 @@ import TulipTasksHeroImage from '../../../public/images/tulipTasks/tulipTasks-he
 const TulipTasksWireframe = () => {
   return (
     <div>
-            <h2 className="font-bold text-8xl mb-8 w-full text-center  sm:text-6xl xs:text-4xl dark:text-light ">
-            Initial UI/UX
+        <h2 className="font-bold text-8xl pb-8 w-full text-center sm:text-6xl xs:text-4xl dark:text-light">
+        User Interviews
         </h2>
-
-        <div className='flex'>
-            <DesignProcess
-                stage={"Brain Dump"}
-                title={"This is the title"}
-                description={"List every possible factor to consider when creating a task on to-do list."}
-                img={TulipTasksHeroImage}
-                link={"/"}
-            />
-            <DesignProcess
-                stage={"Brain Dump"}
-                title={"This is the title"}
-                description={"List every possible factor to consider when creating a task on to-do list."}
-                img={TulipTasksHeroImage}
-                link={"/"}
-            />
-        </div>
+        <AnimatedText text={"User Interviews"}
+                classname='mb-16 sm:mb-8 !text-6xl md:!text-5xl sm:!text-3xl'
+                />
+        <FramerImage
+            src={TulipTasksInterview}
+            alt={"Half the image is a messy desk covered in post it notes. The other half of the image is a laptop on a clean desk open to Tulip Tasks."}
+            className='w-full h-auto  pb-4'
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.3 }}
+        />
+            <h2 className="font-bold text-8xl pb-8 w-full text-center sm:text-6xl xs:text-4xl dark:text-light">
+        UX Wireframing
+        </h2>
+        <FramerImage
+            src={TulipTasksWireFrame}
+            alt={"Half the image is a messy desk covered in post it notes. The other half of the image is a laptop on a clean desk open to Tulip Tasks."}
+            className='w-full h-auto  pb-4'
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.3 }}
+        />
     </div>
   )
 }
