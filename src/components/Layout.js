2dc7733b9b7a1c19ef5ba02978f81acf412faa52
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-08-30 12:48:54 -0400

    Adjusting spacing.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Layout = ({children, className=""}) => {
+const Layout = ({ children, className = "" }) => {
   return (
-    <div 
-    className={`
-    w-full h-full inline-block z-0 bg-light p-96 dark:bg-dark 2xl:p-32 xl:p-24 lg:p-16 md:p-12 sm:p-4 ${className}
-    `}>
-        {children}
+    <div
+      className={`
+      w-full max-w-[1200px] h-full inline-block mx-auto z-0 bg-light px-4 py-16 dark:bg-dark  ${className}
+      `}
+    >
+      {children}
     </div>
-  )
-}
+  );
+};
 
 //px-32 xl:p-32 lg:p-16 md:p-12 sm:p-8 xs:p-8 2xl:px-64 px-64
 
-export default Layout
+export default Layout;
