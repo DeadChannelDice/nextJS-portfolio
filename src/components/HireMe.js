46c5eef68c96b7a6b5d68677a71d9f5517d7eb7b
DeadChannelDice <92655490+DeadChannelDice@users.noreply.github.com>
2024-08-30 15:05:37 -0400

    Updating the spinningtext.

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,33 +1,36 @@
-import React from 'react'
-import { CircularTextID } from './Icons'
-import Link from 'next/link'
+import React from "react";
+import { CircularTextID, CircularTextSoftware } from "./Icons";
+import Link from "next/link";
 
 const HireMe = () => {
   return (
-    <div className='fixed left-4 bottom-4
+    <div
+      className="fixed left-4 bottom-4
     flex items-center justify-center overflow-hidden
     md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
-    '>
-        <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
-            <CircularTextID className={"fill-dark animate-reverse-spin dark:fill-light"} />
+    "
+    >
+      <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
+        <CircularTextSoftware
+          className={"fill-dark animate-reverse-spin dark:fill-light"}
+        />
 
-            <Link href="mailto:Michael.Charpin@gmail.com" 
-              className='
+        <Link
+          href="mailto:Michael.Charpin@gmail.com"
+          className="
                 flex items-center justify-center
                 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
                 bg-dark text-light shadow-md border-2 border-solid border-dark w-20 h-20 rounded-full font-semibold
                 hover:bg-light hover:text-dark
                 dark:border-light
                 md:w-12 md:h-12 md:text-[10px] text-center
-              '>
-                Lets connect
-            </Link>
-        </div>
-        
-        
-        
+              "
+        >
+          Hire me&#129497;
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HireMe
+export default HireMe;
